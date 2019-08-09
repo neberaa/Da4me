95c4dfca42a721bc178085433437f3862de94a65
Nebera Anton <anebera@apextech.com.ua>
2019-08-09 05:02:05 -0600

    - add Page template and PageContent vue components

---
+++ src/components/PageContent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div
+        class="page-content"
+        v-html="content"
+    />
+</template>
+
+<script>
+export default {
+    props: {
+        content: String
+    }
+}
+</script>
+
+<style>
+</style>
---
+++ src/templates/PageItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <Layout>
+    <div class="page">
+      <div class="container page-container">
+        <h1 v-html="$page.page.title" class="page-title" />
+        <page-content :content="$page.page.content" />
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query PageItem ($path: String!) {
+  page: pageItem (path: $path) {
+    title
+    content
+  }
+}
+</page-query>
+
+<script>
+import PageContent from "@/components/PageContent"
+
+export default {
+  components: {
+    PageContent
+  },
+  metaInfo () {
+    return {
+      title: this.$page.page.title
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
