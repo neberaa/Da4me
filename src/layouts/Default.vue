059478d39d836c3e9693ba55909ced810a611b76
Nebera Anton <anebera@apextech.com.ua>
2019-08-23 07:46:40 -0600

    - playing with custom cursor

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="layout sticky-header">
+    <div class="cursor"></div>
+    <div class="cursor-follower"></div>
     <Header />
       <transition name="fade" appear>
         <main class="page-content">
@@ -13,11 +15,64 @@
 <script>
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
+import {TweenMax} from "gsap/TweenMax";
+import $ from 'jquery';
 
 export default {
   components: {
     Header,
     Footer
+  },
+  methods: {
+    initCustomCursor() {
+      const cursor = $(".cursor"),
+        follower = $(".cursor-follower");
+
+      let posX = 0,
+        posY = 0;
+
+      let mouseX = 0,
+        mouseY = 0;
+
+      TweenMax.to({}, 0.016, {
+        repeat: -1,
+        onRepeat: function() {
+          posX += (mouseX - posX) / 9;
+          posY += (mouseY - posY) / 9;
+
+          TweenMax.set(follower, {
+            css: {
+              left: posX - 12,
+              top: posY - 12
+            }
+          });
+
+          TweenMax.set(cursor, {
+            css: {
+              left: mouseX,
+              top: mouseY
+            }
+          });
+        }
+      });
+
+      $(document).on("mousemove", function(e) {
+        mouseX = e.pageX;
+        mouseY = e.pageY;
+      });
+
+      $("[data-lock]").on("mouseenter", function() {
+        cursor.addClass("active");
+        follower.addClass("active");
+      });
+      $("[data-lock]").on("mouseleave", function() {
+        cursor.removeClass("active");
+        follower.removeClass("active");
+      });
+    }
+  },
+  mounted() {
+this.initCustomCursor();
   }
 }
 </script>
@@ -35,6 +90,7 @@ body {
   padding: 0;
   font-size: 16px;
   transition: background 0.5s ease;
+  cursor: none;
   &.scroll-lock {
     @include screenBreakpoint2(desktop) {
       padding-right: 17px;
@@ -75,7 +131,7 @@ button {
   border: none;
   overflow: visible;
   background-color: transparent;
-  cursor: pointer;
+  //cursor: pointer;
   color: inherit;
   font: inherit;
   text-align: inherit;
@@ -127,6 +183,52 @@ main.page-content {
   fill: $gray;
   &--white {
     fill: $white;
+  }
+}
+
+.cursor {
+  position: absolute;
+  background-color: $gray;
+  width: 15px;
+  height: 15px;
+  border-radius: 100%;
+  transition: 0.3s cubic-bezier(0.75, -1.27, 0.3, 2.33) transform,
+  0.2s cubic-bezier(0.75, -0.27, 0.3, 1.33) opacity;
+  user-select: none;
+  pointer-events: none;
+  z-index: 10000;
+  transform: scale(1);
+
+  &.active {
+    opacity: 0.5;
+    transform: scale(0);
+  }
+
+  &.hovered {
+    opacity: 0.08;
+  }
+}
+
+.cursor-follower {
+  position: absolute;
+  background-color: rgba($gray, 0.3);
+  width: 40px;
+  height: 40px;
+  border-radius: 100%;
+  transition: 0.6s cubic-bezier(0.75, -1.27, 0.3, 2.33) transform,
+  0.2s cubic-bezier(0.75, -0.27, 0.3, 1.33) opacity;
+  user-select: none;
+  pointer-events: none;
+  z-index: 10000;
+  transform: translate(0px, 0px);
+
+  &.active {
+    opacity: 0.7;
+    transform: scale(2);
+  }
+
+  &.hovered {
+    opacity: 0.08;
   }
 }
 
