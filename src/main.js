88e21588dacbeeeda09d422e61ff30f80d69d4d9
Nebera Anton <anebera@apextech.com.ua>
2019-08-23 07:09:52 -0600

    - reticool package remove because of a compilation error

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import DefaultLayout from "~/layouts/Default.vue";
 import store from './store';
-import RETICOOL from 'reticool';
-import reticoolConfig from './reticoolConfig'
 
 export default function(Vue, { head, appOptions }) {
   Vue.component("Layout", DefaultLayout);
   head.htmlAttrs = { lang: 'ru' };
   
   appOptions.store = store;
-  new RETICOOL(reticoolConfig);
 }
