edf0cfc58e9d6d9e0e8b8965d71a60b105cda3b7
Nebera Anton <anebera@apextech.com.ua>
2019-08-28 07:49:13 -0600

    - try to fix compilation error

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 import DefaultLayout from "~/layouts/Default.vue";
-import carousel from 'vue-owl-carousel'
 import store from './store';
 
 export default function(Vue, { head, appOptions }) {
   Vue.component("Layout", DefaultLayout);
-  Vue.component(carousel);
   head.htmlAttrs = { lang: 'ru' };
   
   appOptions.store = store;
